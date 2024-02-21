dd8a19e6f12c0e824a6f1148d5c252c11666e1eb
Mashruf Ehsan <mashruf.ehsan@gmail.com>
2024-02-21 20:07:44 +0000

    Done till Serve Static Assets

--- Basic-Node-and-Express/myApp.js
+++ Basic-Node-and-Express/myApp.js
@@ -2,7 +2,28 @@ let express = require('express');
 let app = express();
 
 
+// 1
 
+console.log("Hello World")
+
+/*
+// 2
+
+app.get('/', (req, res)=>{
+    res.send("Hello Express")
+})
+*/
+
+//3
+const absolutePath = __dirname + '/views/index.html'
+
+app.get('/', (req, res)=>{
+    res.sendFile(absolutePath)
+})
+
+//4
+
+app.use('/public', express.static(__dirname + '/public'))
 
 
 
