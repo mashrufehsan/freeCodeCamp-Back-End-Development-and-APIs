4b4bf435197d666f144c7a04dfd1b5c17dfc40f8
Mashruf Ehsan <mashruf.ehsan@gmail.com>
2024-02-21 20:36:13 +0000

    Done till Implement a Root-Level Request Logger Middleware

--- Basic-Node-and-Express/myApp.js
+++ Basic-Node-and-Express/myApp.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-
+require('dotenv').config(); // this is required to access the .env file
 
 // 1
 
@@ -25,23 +25,31 @@ app.get('/', (req, res)=>{
 
 app.use('/public', express.static(__dirname + '/public'))
 
+// 7
 
+app.use((req, res, next)=>{
+    console.log(req.method + ' ' + req.path + ' - ' + req.ip)
+    next()
+})
 
+/*
+//5
+app.get('/json',(req, res)=>{
+    res.json({message: "Hello json"})
+})
+*/
 
+//6
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/json',(req, res)=>{
+    
+    if(process.env.MESSAGE_STYLE == 'uppercase'){
+        res.json({message: 'HELLO JSON'})
+    }
+    else{
+        res.json({message: 'Hello json'})
+    }
+})
 
 
 
